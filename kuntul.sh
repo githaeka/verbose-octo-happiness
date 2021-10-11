67d09b744ea9a5ec77b8bb6f9ff2615ee6f31587
githaeka <92292441+githaeka@users.noreply.github.com>
2021-10-11 14:21:29 +0000

    Update kuntul.sh

--- kuntul.sh
+++ kuntul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/gasken1/asw.git && cd asw
+git clone https://github.com/githaeka/asw.git && cd asw
 chmod +x okeh.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
